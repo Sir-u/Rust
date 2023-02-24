adfc1d29be7781e42028b0dbebbaa9979d924977
Siru <ehdgur9553@naver.com>
2023-02-24 12:47:05 +0900

    connert test

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,8 +1,15 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The Secret number is: {}", secret_number);
+    
     println!("Please input your quess.");
 
     let mut quess = String::new();
